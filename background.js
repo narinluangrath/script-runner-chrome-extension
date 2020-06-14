b716d7531605a0c7e7c82ebed1b6824b277046b1
Narin Luangrath <narin.luangrath@gmail.com>
2020-06-13 20:07:50 -0700

    Make code more organized, better separation of concerns

--- background.js
+++ background.js
@@ -1,6 +1,22 @@
-console.log("Running background.js");
+function Main() {
+  chrome.runtime.onMessage.addListener(handleMessage);
+}
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  const jsUrls = request.jsUrls;
+function parseIndexFile(payload) {
+  const jsUrls = payload;
   jsUrls.forEach((url) => chrome.tabs.create({ url }));
-});
+}
+
+function handleMessage(request, sender, sendResponse) {
+  const { type, payload } = request;
+
+  switch (type) {
+    case "INDEX_FILE":
+      parseIndexFile(payload);
+      break;
+    default:
+      console.error(`No handler for type: ${type}`);
+  }
+}
+
+Main();
--- content.js
+++ content.js
@@ -1,7 +1,24 @@
-console.log("Running content.js");
-const anchorEls = Array.from(document.getElementsByTagName("a"));
-const fileUrls = anchorEls.map((anchorEl) => anchorEl.getAttribute("href"));
-const jsUrls = fileUrls
-  .filter((url) => url.endsWith(".js"))
-  .map((url) => `file://${url}`);
-chrome.runtime.sendMessage({ jsUrls });
+function SendJSFilesFromIndexFile() {
+  const anchorEls = Array.from(document.getElementsByTagName("a"));
+  const fileUrls = anchorEls.map((anchorEl) => anchorEl.getAttribute("href"));
+  const jsUrls = fileUrls
+    .filter((url) => url.endsWith("sr.js"))
+    .map((url) => `file://${url}`);
+  chrome.runtime.sendMessage({ type: "INDEX_FILE", payload: jsUrls });
+}
+
+function Main() {
+  const url = document.URL;
+  const isScriptRunnerDir = url.endsWith("script-runner-scripts/");
+  const isScriptRunnerScript = url.endsWith("sr.js");
+
+  if (isScriptRunnerDir) {
+    SendJSFilesFromIndexFile();
+  } else if (isScriptRunnerScript) {
+    // Parse the file, save it to storage, close the file
+  } else {
+    // Normal HTML Document. Check to see if scripts match.
+  }
+}
+
+Main();
